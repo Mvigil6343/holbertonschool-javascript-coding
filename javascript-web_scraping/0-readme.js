c6a9543ecca0681259c343dcd9b65ce064a5f174
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 <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>
2024-07-09 00:00:27 -0700

    script that reads and prints the content of a file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+  console.error('Please provide the file path as an argument');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
